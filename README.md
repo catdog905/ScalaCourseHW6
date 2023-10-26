f585fc2a3d6a7ce4c650af7c32865730c5cc423a
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2023-10-26 21:14:15 +0300

    Fixed incorrect multiplication operation

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 final case class ComplexNumber(real: Double, imaginary: Double) {
   def *(other: ComplexNumber) =
     ComplexNumber(
-      (real * other.real) + (imaginary * other.imaginary),
+      (real * other.real) - (imaginary * other.imaginary),
       (real * other.imaginary) + (imaginary * other.real)
     )
 
--- src/main/scala/complex/ComplexNumber.scala
+++ src/main/scala/complex/ComplexNumber.scala
@@ -4,7 +4,7 @@ package complex
 final case class ComplexNumber(real: Double, imaginary: Double) {
   def *(other: ComplexNumber) =
     ComplexNumber(
-      (real * other.real) + (imaginary * other.imaginary),
+      (real * other.real) - (imaginary * other.imaginary),
       (real * other.imaginary) + (imaginary * other.real)
     )
   def +(other: ComplexNumber) =
